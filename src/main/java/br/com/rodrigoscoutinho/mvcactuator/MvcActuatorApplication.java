5c629066050ad1468e2a6323954909f7f8522309
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-31 22:50:37 -0300

    first commit

---
+++ src/main/java/br/com/rodrigoscoutinho/mvcactuator/MvcActuatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoscoutinho.mvcactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcActuatorApplication.class, args);
+	}
+
+}
